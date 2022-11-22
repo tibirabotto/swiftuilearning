29631e98428d35101bf36ff105da738cdbfa1dd2
Tibira Botto <tibira.botto@gmail.com>
2022-11-22 10:13:42 -0500

    ScrollViewReader to auto scroll in SwiftUI

---
+++ ScrollViewReaderBootcamp/ScrollViewReaderBootcamp/ScrollViewReaderBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderBootcampApp.swift
+//  ScrollViewReaderBootcamp
+//
+//  Created by Tibirica Neto on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUILearning/SwiftUILearning/ScrollViewReaderBootcamp.swift
@@ -0,0 +1,61 @@
+//
+//  ScrollViewReaderBootcamp.swift
+//  SwiftUILearning
+//
+//  Created by Tibirica Neto on 2022-11-22.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootcamp: View {
+    
+    @State var scrollToIndex: Int = 0
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter a # here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("SCROLL NOW") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
+                    }
+                }
+                
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 20)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { newValue in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(newValue, anchor: .top)
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReaderBootcamp()
+    }
+}

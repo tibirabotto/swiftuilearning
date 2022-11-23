2361006809c429ad6f121de0a2cf573b90bfac39
Tibira Botto <tibira.botto@gmail.com>
2022-11-23 10:21:48 -0500

    How to add sound effects to Xcode project

--- SwiftUILearning/SwiftUILearning/MaskBootcamp.swift
+++ SwiftUILearning/SwiftUILearning/MaskBootcamp.swift
@@ -38,7 +38,8 @@ struct MaskBootcamp: View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Rectangle()
-                    .foregroundColor(.yellow)
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                    //.foregroundColor(.yellow)
                     .frame(width: CGFloat(rating) / 5 * geometry.size.width)
                     
             }
---
+++ SwiftUILearning/SwiftUILearning/SoundsBootcamp.swift
@@ -0,0 +1,56 @@
+//
+//  SoundsBootcamp.swift
+//  SwiftUILearning
+//
+//  Created by Tibirica Neto on 2022-11-23.
+//
+
+import SwiftUI
+import AVKit
+
+class SoundManager {
+   static let instance = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    enum SoundOption: String {
+        case tada
+        case badum
+    }
+    
+    func playSound(sound: SoundOption) {
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else {return}
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("ERROR playing sound. \(error.localizedDescription)")
+        }
+       
+    }
+    
+
+}
+
+struct SoundsBootcamp: View {
+    
+    
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            Button("PLAY SOUND") {
+                SoundManager.instance.playSound(sound: .tada)
+            }
+            Button("PLAY SOUND 2") {
+                SoundManager.instance.playSound(sound: .badum)
+            }
+        }
+    }
+}
+
+struct SoundsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SoundsBootcamp()
+    }
+}
